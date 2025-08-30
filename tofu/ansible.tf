6b8c18222b3f8001a818bb35571e4920da50bcc7
Paul Bort on CentOS 10 <contact@blacksilverconsulting.com>
2025-08-30 10:37:01 -0400

    more alignment with Ansible

--- tofu/ansible.tf
+++ tofu/ansible.tf
@@ -2,8 +2,8 @@ resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../ansible/hosts"
   content  = <<EOF
 [minecraft]
-lotuscove ansible_host=${digitalocean_droplet.lotuscove.ipv4_address}
-${var.backup_id != null ? "lotuscove-test ansible_host=${digitalocean_droplet.lotuscove-test[0].ipv4_address}" : ""}
+${local.project} ansible_host=${digitalocean_droplet.lotuscove.ipv4_address}
+${var.backup_id != null ? "${local.project}-test ansible_host=${digitalocean_droplet.lotuscove-test[0].ipv4_address}" : ""}
 EOF
 
   depends_on = [
@@ -17,6 +17,12 @@ resource "local_file" "group_vars" {
   filename = "${path.module}/../ansible/group_vars/minecraft.yaml"
   content  = <<EOF
 console_port: "${local.port.console}"
+project: "${local.project}"
+minecraft_user: "minecraft"
+minecraft_group: "{{ minecraft_user }}"
+minecraft_server_root: "/opt/minecraft"
+minecraft_server_dir: "{{ minecraft_root }}/{{ project }}"
+minecraft_propfile: "server.properties"
 EOF
 
   depends_on = [
@@ -24,3 +30,16 @@ EOF
   ]
   file_permission = "0744"
 }
+
+resource "local_file" "host_vars" {
+  filename = "${path.module}/../ansible/host_vars/${local.project}"
+  content = <<EOF
+server_port: "${local.port.mc}"
+EOF
+
+  depends_on = [
+    digitalocean_firewall.lotuscove-prod
+  ]
+  file_permission = "0744"
+}
+
