2f24780bb9750e8e5eb5950f31d713531806b91a
Paul Bort on CentOS 10 <contact@blacksilverconsulting.com>
2025-09-27 08:50:57 -0400

    Add warning comments to generated files

--- tofu/ansible.tf
+++ tofu/ansible.tf
@@ -1,9 +1,11 @@
 resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../ansible/hosts"
   content  = <<EOF
+### BEGIN content from OpenTofu, do not edit manually ###
 [minecraft]
 ${local.project} ansible_host=${digitalocean_record.minecraft_a.fqdn}
 ${var.backup_id != null ? "${local.project}-test ansible_host=${digitalocean_droplet.test[0].ipv4_address}" : ""}
+### END content from OpenTofu, do not edit manually ###
 EOF
 
   depends_on = [
@@ -16,9 +18,11 @@ EOF
 resource "local_file" "group_vars" {
   filename = "${path.module}/../ansible/group_vars/minecraft.yaml"
   content  = <<EOF
+### BEGIN content from OpenTofu, do not edit manually ###
 console_port: "${local.port.console}"
 project: "${local.project}"
 dns_name: "${digitalocean_record.minecraft_a.fqdn}"
+### END content from OpenTofu, do not edit manually ###
 EOF
 
   depends_on = [
@@ -30,7 +34,9 @@ EOF
 resource "local_file" "host_vars" {
   filename = "${path.module}/../ansible/host_vars/${local.project}"
   content  = <<EOF
+### BEGIN content from OpenTofu, do not edit manually ###
 server_port: "${local.port.mc}"
+### END content from OpenTofu, do not edit manually ###
 EOF
 
   depends_on = [
